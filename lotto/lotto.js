d0aa7afdc884a6f661633c13e79e716914ef3436
devgyuri <64848608+devgyuri@users.noreply.github.com>
2022-06-30 18:58:45 +0900

    lotto
    first version

---
+++ lotto/lotto.js
@@ -0,0 +1,50 @@
+const candidate = Array(45).fill().map((v, i) => i + 1);
+const shuffle = [];
+while(candidate.length > 0) {
+  const random = Math.floor(Math.random() * candidate.length); // index random으로 뽑기
+  const spliceArray = candidate.splice(random, 1);
+  const value = spliceArray[0]; // random index의 값을 빼내어 shuffle에 넣기
+  shuffle.push(value);
+}
+console.log(shuffle);
+const winBalls = shuffle.slice(0, 6).sort((a, b) => a - b); 
+const bonus = shuffle[6];
+console.log(winBalls, bonus);
+
+const $result = document.querySelector('#result');
+const $bonus = document.querySelector('#bonus');
+const colorBall = (number, $ball) => {
+  let color;
+  let textColor = 'black';
+  if(number < 10) {
+    color = 'red';
+    textColor = 'white';
+  }
+  else if(number < 20) {
+    color = 'gold';
+  }
+  else if(number < 30) {
+    color = 'yellowgreen';
+  }
+  else {
+    color = 'dodgerblue';
+    textColor = 'white';
+  }
+  $ball.style.backgroundColor = color;
+  $ball.style.color = textColor;
+}
+const drawBall = (number, $target) => {
+  const $ball = document.createElement('div');
+  $ball.className = 'ball';
+  $ball.textContent = number;
+  colorBall(number, $ball);
+  $target.appendChild($ball);
+};
+for(let i=0; i<6; i++) {
+  setTimeout(() => {
+    drawBall(winBalls[i], $result);
+  }, 1000*(i+1));
+}
+setTimeout(() => {
+  drawBall(bonus, $bonus);
+}, 7000);
